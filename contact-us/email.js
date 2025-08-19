bd1920e741d5b6df94fd507d60e3421108d0daef
16BitDevelopment <16bitdevelopment@gmail.com>
2025-08-19 10:38:22 +1000

    Privacy policy added

--- contact-us/email.js
+++ contact-us/email.js
@@ -9,19 +9,23 @@ formEl.addEventListener("submit", (event) => {
 
     emailProgressEl.innerText = "Sending...";
 
-    const nameVal = document.getElementById("form-name").value;
-    const emailVal = document.getElementById("form-email").value;
-    const messageVal = document.getElementById("form-message").value;
+    let nameVal = document.getElementById("form-name");
+    let emailVal = document.getElementById("form-email");
+    let messageVal = document.getElementById("form-message");
 
     const params = {
-        sender: nameVal,
-        email: emailVal,
-        message: messageVal
+        sender: nameVal.value,
+        email: emailVal.value,
+        message: messageVal.value
     };
 
     emailjs.send("service_w00x6ci", "template_qt9ud8o", params).then(() => {
         emailLoadingEl.classList.remove("active");
 
         emailProgressEl.innerText = "Sent";
+
+        nameVal.value = "";
+        emailVal.value = "";
+        messageVal.value = "";
     });
 });
